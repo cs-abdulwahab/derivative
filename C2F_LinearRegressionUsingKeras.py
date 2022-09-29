a9437b7021d1feab66ad889ec904408bd695ffa3
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2022-09-29 07:36:37 +0500

    Linear Regression using Keras Step By Step using
    https://youtu.be/oGuCxVyEhiA

--- C2F_LinearRegressionUsingKeras.py
+++ C2F_LinearRegressionUsingKeras.py
@@ -9,3 +9,14 @@ def c2F(c):
 
 C = tf.random.uniform(shape=(50,), minval=-40, maxval=40, seed=1, name="C")
 F = np.array(c2F(C))
+
+l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
+
+model = tf.keras.models.Sequential(l0)
+# model.add(tf.keras.Input(shape=(1,)))
+
+model.compile(loss=tf.keras.losses.mse, optimizer=tf.keras.optimizers.Adam(0.1))
+
+history = model.fit(x = C , y=F , epochs=500)
+
+
